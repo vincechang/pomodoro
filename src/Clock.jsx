e5b8e9458bd9a938c0886bdcb19d867a74e8256a
Vincent Chang <vincentchang402@gmail.com>
2017-04-18 18:00:42 +0800

    Add angle of clock hands

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 class Clock extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: new Date(),
+      now: moment(),
     };
   }
 
@@ -20,18 +21,23 @@ class Clock extends Component {
 
   tick() {
     this.setState({
-      date: new Date(),
+      now: moment(),
     });
   }
 
   render() {
+    const t = this.state.now;
+    const second = t.second();
+    const minute = t.minutes();
+    const hour = t.hour() % 12;
+    const angleOfSecond = (second / 60) * 360;
+    const angleOfMinute = ((minute / 60) * 360) + angleOfSecond;
+    const angleOfHour = ((hour / 12) * 360) + angleOfMinute;
     return (
-      <div>
-        <div>
-          {this.state.date.toString()}
-        </div>
-        <div>
-          {this.state.comment}
+      <div className="container">
+        <div className="clock">
+          <div className="hour" />
+          <div className="minute" />
         </div>
       </div>
     );
