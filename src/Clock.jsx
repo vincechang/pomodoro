060455868c098557b01916d1065a72f5905d4f43
Vincent Chang <vincentchang402@gmail.com>
2017-04-18 23:53:19 +0800

    Clock status progressing

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -6,7 +6,17 @@ class Clock extends Component {
     super(props);
     this.state = {
       now: moment(),
+      clockStatus: 'normal',
+      pomodorosDone: 0,
+      pomodorosPerSet: 4,
+      pomodoroMinutes: 25,
+      shortBreakMinutes: 5,
+      longBreakMinutes: 20,
     };
+    this.clickClock = this.clickClock.bind(this);
+    this.setWorking = this.setWorking.bind(this);
+    this.cancelWorking = this.cancelWorking.bind(this);
+    this.cancelBreak = this.cancelBreak.bind(this);
   }
 
   componentDidMount() {
@@ -19,10 +29,37 @@ class Clock extends Component {
     clearInterval(this.timeID);
   }
 
+  setWorking() {
+    console.log('working');
+    this.setState({ clockStatus: 'working' });
+  }
+
+  cancelWorking() {
+    console.log('cancel working');
+    this.setState({ clockStatus: 'normal' });
+  }
+
+  cancelBreak() {
+    console.log('cancel break');
+    this.setState({ clockStatus: 'normal' });
+  }
+
   tick() {
     this.setState({
       now: moment(),
     });
+  }
+
+  clickClock() {
+    const clockStatus = this.state.clockStatus;
+    const action = {
+      normal: this.setWorking,
+      working: this.cancelWorking,
+      break: this.cancelBreak,
+    };
+    if (clockStatus in action) {
+      action[clockStatus]();
+    }
   }
 
   render() {
@@ -45,13 +82,12 @@ class Clock extends Component {
       },
     };
     return (
-      <div className="clock">
+      <div className="clock" onClick={this.clickClock}>
         <div className="top" />
         <div className="right" />
         <div className="bottom" />
         <div className="left" />
         <div className="center" />
-        <div className="shadow" />
         <div className="hands second" style={style.secondAngle} />
         <div className="hands minute" style={style.minuteAngle} />
         <div className="hands hour" style={style.hourAngle} />
